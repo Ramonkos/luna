348414de229bad315d3fea235c6296ed243386e0
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-07-05 11:12:31 +0200

    Added Placeholder Images and BaseUrl to a created constant.js file.

---
+++ frontend/src/constants.js
@@ -0,0 +1,14 @@
+// Use for default User Avatar on User Cards:
+// `https://eu.ui-avatars.com/api/?name=${user.first_name}+${user.last_name}`
+
+export const baseUrl = 'http://127.0.0.1:8000/backend/api/'; // TODO Set this to proper URL for Deployment
+
+export const defaultLandingPageBanner = 'https://via.placeholder.com/1440x351'; // 1440 x 351
+export const defaultRestaurantReviewBanner = 'https://via.placeholder.com/1440x204'; // 1440 x 204
+export const defaultUserProfileBanner = 'https://via.placeholder.com/1440x155'; // 1440 x 155
+export const defaultRestaurantPageBanner = 'https://via.placeholder.com/1440x496'; // 1440 x 496
+export const defaultRestaurantCardPicture = 'https://via.placeholder.com/271x284'; // 271 x 284
+export const defaultUserProfileAvatar = 'https://via.placeholder.com/232x234'; // 232 x 234
+
+
+
