c393003fc50d734cbfbf4d06c94fa8000e28801d
ramon_koestli <ramonkos@gmail.com>
2020-07-06 15:52:11 +0200

    remove styles.js

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,10 +1,45 @@
 import React from "react";
 import { Link } from 'react-router-dom'
 
-import { HeaderContainer } from "./style.js";
-import { LinkButtonWrapper } from "./style.js";
+// import { HeaderContainer } from "./style.js";
+// import { LinkButtonWrapper } from "./style.js";
 import { SmallOrangeLeftButton } from "../../../src/style/GlobalButton"
 import { SmallOrangeRightButton } from "../../../src/style/GlobalButton"
+import styled from "styled-components";
+import { rem } from "polished";
+
+//TODO: choose H1 font and add .active link
+export const HeaderContainer = styled.div`
+  border: solid 1px white;
+  width: 100%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #fff;
+  h1 {
+    padding-left: 30px;
+    font-family: 'Merienda One', cursive, Arial, serif;;
+    font-size: ${rem('36px')};
+    color: #000;
+  }
+`;
+
+
+//TODO: active Links
+export const LinkButtonWrapper = styled.div`
+  width: 70%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding-right: 30px;
+  a {
+    padding-right: 70px;
+    font-size: ${rem('20px')};
+    line-height: ${rem('23px')};
+    color: #4A4A4A;
+  }
+`;
 
 //TODO: change a to link
 const Header = () => {
