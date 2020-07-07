b209f62235a23ac1ef905f34cdb9f7db791d12ad
ramon_koestli <ramonkos@gmail.com>
2020-07-07 21:28:27 +0200

    added button

--- frontend/src/components/UserProfileRestaurant/index.js
+++ frontend/src/components/UserProfileRestaurant/index.js
@@ -1,52 +1,69 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import { UserProfileTitle } from "../../style/GlobalTitles";
+import { Button } from "../../style/GlobalButton";
 import {
   CommentContainer,
   CommentWrapper,
   ReviewCommentWrapper,
   TitleWrapper,
   TitleRestaurant,
-  StarWrapper
+  StarWrapper,
 } from "../../style/GlobalUserReviewRestaurant";
 
-const UserProfileRestaurant = () => {
+const ButtonRestaurantContainer = styled.div`
+  width: 620px;
+`;
 
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 25px;
+`;
+
+const UserProfileRestaurant = () => {
   return (
-    <CommentContainer>
-      <TitleWrapper>
-        <UserProfileTitle>Restaurants</UserProfileTitle>
-      </TitleWrapper>
-      <CommentWrapper>
-        <ReviewCommentWrapper>
-          <TitleRestaurant>
-            <a>Läderach Chocolatier Suisse</a>
-            <p>02.03.2020 22:29</p>
-          </TitleRestaurant>
-          <StarWrapper>★★★★★</StarWrapper>
-          <p>
-            Monotonectally productize virtual internal or "organic" sources
-            vis-a-vis error-free opportunities. Holisticly disintermediate
-            market-driven deliverables through integrated human capital.
-            Continually.
-          </p>
-        </ReviewCommentWrapper>
-      </CommentWrapper>
-      <CommentWrapper>
-        <ReviewCommentWrapper>
-          <TitleRestaurant>
-            <a>Läderach Chocolatier Suisse</a>
-            <p>02.03.2020 22:29</p>
-          </TitleRestaurant>
-          <StarWrapper>★★★★★</StarWrapper>
-          <p>
-            Efficiently repurpose interoperable internal or "organic" sources
-            through transparent alignments. Holisticly incubate.
-          </p>
-        </ReviewCommentWrapper>
-      </CommentWrapper>
-    </CommentContainer>
+    <ButtonRestaurantContainer>
+      <CommentContainer>
+        <TitleWrapper>
+          <UserProfileTitle>Restaurants</UserProfileTitle>
+        </TitleWrapper>
+        <CommentWrapper>
+          <ReviewCommentWrapper>
+            <TitleRestaurant>
+              <a>Läderach Chocolatier Suisse</a>
+              <p>02.03.2020 22:29</p>
+            </TitleRestaurant>
+            <StarWrapper>★★★★★</StarWrapper>
+            <p>
+              Monotonectally productize virtual internal or "organic" sources
+              vis-a-vis error-free opportunities. Holisticly disintermediate
+              market-driven deliverables through integrated human capital.
+              Continually.
+            </p>
+          </ReviewCommentWrapper>
+        </CommentWrapper>
+        <CommentWrapper>
+          <ReviewCommentWrapper>
+            <TitleRestaurant>
+              <a>Läderach Chocolatier Suisse</a>
+              <p>02.03.2020 22:29</p>
+            </TitleRestaurant>
+            <StarWrapper>★★★★★</StarWrapper>
+            <p>
+              Efficiently repurpose interoperable internal or "organic" sources
+              through transparent alignments. Holisticly incubate.
+            </p>
+          </ReviewCommentWrapper>
+        </CommentWrapper>
+      </CommentContainer>
+      <ButtonWrapper>
+        <Button>Create Restaurant</Button>
+      </ButtonWrapper>
+    </ButtonRestaurantContainer>
   );
 };
 
