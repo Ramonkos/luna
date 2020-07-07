8dc5f0eb77ea07a5910a977078d7c2e935d7c266
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-07-07 12:11:23 +0200

    Landing Page Views done

--- backend/landingpage/views.py
+++ backend/landingpage/views.py
@@ -1,13 +1,48 @@
+from django.contrib.auth import get_user_model
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from landingpage.filters import DynamicSearchFilter
+from landingpage.serializers import CategorySerializer
+from restaurants.models import Restaurant
+from restaurants.serializers import RestaurantSerializer
+from reviews.models import Review
+from reviews.serializers import ReviewSerializer
+from users.serializers import UserSerializer
 
 
 class ListRestaurantCategoriesView(ListAPIView):
-    pass
+    serializer_class = CategorySerializer
+    queryset = Restaurant.objects.distinct('category').all()
 
 
 class SearchRestaurantsReviewsUsersView(ListAPIView):
-    pass
+    filter_backends = (DynamicSearchFilter,)
+
+    def get_queryset(self):
+        if self.request.query_params['search_location'] == 'restaurants':
+            return Restaurant.objects.all()
+        elif self.request.query_params['search_location'] == 'reviews':
+            return Review.objects.all()
+        else:
+            User = get_user_model()
+            return User.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.query_params['search_location'] == 'restaurants':
+            return RestaurantSerializer
+        elif self.request.query_params['search_location'] == 'reviews':
+            return ReviewSerializer
+        else:
+            return UserSerializer
 
 
 class ListTopFourRestaurantsView(ListAPIView):
-    pass
+    serializer_class = RestaurantSerializer
+    queryset = Restaurant.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        serializer_data = serializer.data
+        serializer_data = sorted(serializer_data, key=lambda k: k['review_average'], reverse=True)
+        return Response(serializer_data[:4])
--- backend/reviews/views.py
+++ backend/reviews/views.py
@@ -1,8 +1,23 @@
+from rest_framework import status
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView, GenericAPIView
+from rest_framework.response import Response
+
+from restaurants.models import Restaurant
+from reviews.models import Review
+from reviews.serializers import ReviewSerializer
 
 
 class CreateReviewView(CreateAPIView):
-    pass
+    serializer_class = ReviewSerializer
+    lookup_url_kwarg = 'restaurant_id'
+    queryset = Restaurant
+
+    def create(self, request, *args, **kwargs):
+        restaurant = self.get_object()
+        review = Review.objects.create(text_content=request.data['text_content'], restaurant_review_about=restaurant,
+                                       author=request.user, rating=request.data['rating'])
+        serializer = self.get_serializer(review)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class ListReviewCommentsView(ListAPIView):
