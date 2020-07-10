47b5b989e6ed5db3becb35f00d77733dac504346
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-07-10 15:23:16 +0200

    setting up deploy state

--- backend/luna_project/settings.py
+++ backend/luna_project/settings.py
@@ -64,8 +64,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
-    "http://127.0.0.1:8000"
+    # "http://localhost:3000",
+    # "http://127.0.0.1:8000"
 ]
 
 ROOT_URLCONF = 'luna_project.urls'
