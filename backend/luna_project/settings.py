6ab66595640b97f977cee39ef3dd8ec09394deb8
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-07-10 15:16:49 +0200

    setting up deploy state

--- backend/luna_project/settings.py
+++ backend/luna_project/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k3(-4f4s0tfg@ran#r3(h1!3#&4%3^btj@itm(*fdmu4n-ii#l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = ast.literal_eval(os.environ.get('DJANGO_DEBUG'))
-DEBUG = True
+DEBUG = ast.literal_eval(os.environ.get('DJANGO_DEBUG'))
+# DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
